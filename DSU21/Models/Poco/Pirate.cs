20864a7f32c6e1d4ef9897fa2071f02b1e02940e
Tjejmie <jamie_svanberg@hotmail.com>
2022-01-18 12:13:36 +0100

    Add project files.

---
+++ DSU21/Models/Poco/Pirate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSU21.Models
+{
+    public class Pirate
+    {
+        public int Id { get; set; } //Måste heta Id för att databasen ska förstå att det är ett Id
+        public string Name { get; set; }
+        public int Level { get; set; }
+
+    }
+}
